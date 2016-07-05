237267eba0625ba035854ad2df8c99058caf1291
Andy Pierz <andy@andypierz.com>
2016-07-05 14:45:16 -0700

    Flycheck refactoring

---
+++ emacs.d/elpa/dumb-jump-20160609.2247/dumb-jump-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dumb-jump" "20160609.2247" "jump to definition for multiple languages without configuration." '((emacs "24.3") (f "0.17.3") (s "1.11.0") (dash "2.9.0") (popup "0.5.3")) :keywords '("programming"))
---
+++ emacs.d/elpa/smooth-scrolling-20160612.141/smooth-scrolling-pkg.el
@@ -0,0 +1,1 @@
+(define-package "smooth-scrolling" "20160612.141" "Make emacs scroll smoothly" 'nil :url "http://github.com/aspiers/smooth-scrolling/" :keywords '("convenience"))
