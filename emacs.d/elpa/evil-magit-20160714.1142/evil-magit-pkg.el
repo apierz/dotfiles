0f96b3b73bde18d1f257b5fd655cbaba2d7b1440
Andy Pierz <andy@andypierz.com>
2016-08-01 11:28:30 -0700

    Update some elpa packages

---
+++ emacs.d/elpa/evil-magit-20160714.1142/evil-magit-pkg.el
@@ -0,0 +1,1 @@
+(define-package "evil-magit" "20160714.1142" "evil-based key bindings for magit" '((evil "1.2.3") (magit "2.6.0")) :url "https://github.com/justbur/evil-magit")
---
+++ emacs.d/elpa/web-mode-20160717.1201/web-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "web-mode" "20160717.1201" "major mode for editing web templates" 'nil :url "http://web-mode.org" :keywords '("languages"))
