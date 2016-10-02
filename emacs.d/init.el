4dc6ca786766e9671130ee2e541a9e9399dd9349
Andy Pierz <andy@andypierz.com>
2016-10-02 14:32:13 -0700

    minor tweak

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -23,4 +23,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markdown-url-face ((t (:foreground "#ECBE7B" :weight normal)))))
