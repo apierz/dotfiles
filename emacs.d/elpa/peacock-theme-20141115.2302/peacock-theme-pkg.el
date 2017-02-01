01c54d65c14d48803c3d1f7dc693f46b9b7caadd
Andy Pierz <andy@andypierz.com>
2017-02-01 14:09:38 -0800

    update tao theme

---
+++ emacs.d/elpa/peacock-theme-20141115.2302/peacock-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "peacock-theme" "20141115.2302" "an Emacs 24 theme based on Peacock (tmTheme)" '((emacs "24.0")) :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
