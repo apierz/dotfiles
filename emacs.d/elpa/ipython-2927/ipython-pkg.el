63dd784b2c429d10b98c4fbd06285730fd43e7e8
Andy Pierz <andy@andypierz.com>
2017-05-07 13:28:05 -0700

    emacs update 07may17

---
+++ emacs.d/elpa/ipython-2927/ipython-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ipython" "2927" "Adds support for IPython to python-mode.el" 'nil)
---
+++ emacs.d/elpa/pyvenv-20170224.538/pyvenv-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pyvenv" "20170224.538" "Python virtual environment interface" 'nil :url "http://github.com/jorgenschaefer/pyvenv" :keywords '("python" "virtualenv" "tools"))
