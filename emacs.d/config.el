b00fc9d7c6c25df5dcdb5afa1572cc93797a5158
Andy Pierz <andy@andypierz.com>
2019-04-21 14:52:40 -0700

    vcs testing

--- emacs.d/config.el
+++ emacs.d/config.el
@@ -298,6 +298,9 @@ Repeated invocations toggle between the two most recently open buffers."
 (key-chord-define evil-replace-state-map "jk" 'evil-normal-state)
 (key-chord-define evil-visual-state-map "jk" 'evil-normal-state)
 (key-chord-define evil-motion-state-map "jk" 'evil-normal-state)
+
+(key-chord-define-global "jk" 'evil-normal-state)
+
 (evil-define-key 'normal dired-mode-map "h" 'dired-up-directory)
 (evil-define-key 'normal dired-mode-map "l" 'dired-find-alternate-file)
 (evil-define-key 'normal dired-mode-map "v" 'dired-toggle-marks)
@@ -573,6 +576,8 @@ Repeated invocations toggle between the two most recently open buffers."
 (global-display-line-numbers-mode t)
 (setq display-line-numbers-type 'relative)
 (setq display-line-numbers "%4d \u2502 ")
+
+(set-face-attribute 'line-number-current-line nil :weight 'bold)
 
 (use-package smooth-scrolling
   :config
@@ -909,8 +914,6 @@ Repeated invocations toggle between the two most recently open buffers."
   (imagemagick-register-types))
 (setq mu4e-view-prefer-html nil)
 
-(setq mu4e-html2text-command 'mu4e-shr2text)
-
 (use-package magit)
 (use-package evil-magit)
 
@@ -919,6 +922,103 @@ Repeated invocations toggle between the two most recently open buffers."
 
 (require 'doom-modeline)
 (doom-modeline-mode 1)
+
+;; How tall the mode-line should be (only respected in GUI Emacs).
+(setq doom-modeline-height 25)
+
+;; How wide the mode-line bar should be (only respected in GUI Emacs).
+(setq doom-modeline-bar-width 3)
+
+;; Determines the style used by `doom-modeline-buffer-file-name'.
+;;
+;; Given ~/Projects/FOSS/emacs/lisp/comint.el
+;;   truncate-upto-project => ~/P/F/emacs/lisp/comint.el
+;;   truncate-from-project => ~/Projects/FOSS/emacs/l/comint.el
+;;   truncate-with-project => emacs/l/comint.el
+;;   truncate-except-project => ~/P/F/emacs/l/comint.el
+;;   truncate-upto-root => ~/P/F/e/lisp/comint.el
+;;   truncate-all => ~/P/F/e/l/comint.el
+;;   relative-from-project => emacs/lisp/comint.el
+;;   relative-to-project => lisp/comint.el
+;;   file-name => comint.el
+;;   buffer-name => comint.el<2> (uniquify buffer name)
+;;
+;; If you are expereicing the laggy issue, especially while editing remote files
+;; with tramp, please try `file-name' style.
+;; Please refer to https://github.com/bbatsov/projectile/issues/657.
+(setq doom-modeline-buffer-file-name-style 'truncate-upto-project)
+
+;; Whether display icons in mode-line or not.
+(setq doom-modeline-icon t)
+
+;; Whether display the icon for major mode. It respects `doom-modeline-icon'.
+(setq doom-modeline-major-mode-icon t)
+
+;; Whether display color icons for `major-mode'. It respects
+;; `doom-modeline-icon' and `all-the-icons-color-icons'.
+(setq doom-modeline-major-mode-color-icon t)
+
+;; Whether display icons for buffer states. It respects `doom-modeline-icon'.
+(setq doom-modeline-buffer-state-icon t)
+
+;; Whether display buffer modification icon. It respects `doom-modeline-icon'
+;; and `doom-modeline-buffer-state-icon'.
+(setq doom-modeline-buffer-modification-icon t)
+
+;; Whether display minor modes in mode-line or not.
+(setq doom-modeline-minor-modes nil)
+
+;; If non-nil, a word count will be added to the selection-info modeline segment.
+(setq doom-modeline-enable-word-count nil)
+
+;; If non-nil, only display one number for checker information if applicable.
+(setq doom-modeline-checker-simple-format t)
+
+;; The maximum displayed length of the branch name of version control.
+(setq doom-modeline-vcs-max-length 12)
+
+;; Whether display perspective name or not. Non-nil to display in mode-line.
+(setq doom-modeline-persp-name t)
+
+;; Whether display `lsp' state or not. Non-nil to display in mode-line.
+(setq doom-modeline-lsp t)
+
+;; Whether display github notifications or not. Requires `ghub` package.
+(setq doom-modeline-github nil)
+
+;; The interval of checking github.
+(setq doom-modeline-github-interval (* 30 60))
+
+;; Whether display environment version or not
+(setq doom-modeline-env-version t)
+;; Or for individual languages
+(setq doom-modeline-env-enable-python t)
+(setq doom-modeline-env-enable-ruby t)
+(setq doom-modeline-env-enable-perl t)
+(setq doom-modeline-env-enable-go t)
+(setq doom-modeline-env-enable-elixir t)
+(setq doom-modeline-env-enable-rust t)
+
+;; Change the executables to use for the language version string
+(setq doom-modeline-env-python-executable "python")
+(setq doom-modeline-env-ruby-executable "ruby")
+(setq doom-modeline-env-perl-executable "perl")
+(setq doom-modeline-env-go-executable "go")
+(setq doom-modeline-env-elixir-executable "iex")
+(setq doom-modeline-env-rust-executable "rustc")
+
+;; Whether display mu4e notifications or not. Requires `mu4e-alert' package.
+(setq doom-modeline-mu4e nil)
+
+;; Whether display irc notifications or not. Requires `circe' package.
+(setq doom-modeline-irc nil)
+
+;; Function to stylize the irc buffer names.
+;; (setq doom-modeline-irc-stylize 'identity)
+
+;;Visual Customization
+(set-face-attribute 'doom-modeline-buffer-modified nil :foreground "#FF5555" :background "#AEEEEE")
+(set-face-attribute 'doom-modeline-evil-insert-state nil :foreground "#FF5555" :background "#AEEEEE")
 
 (use-package git-gutter)
 
@@ -953,6 +1053,6 @@ Repeated invocations toggle between the two most recently open buffers."
 
 
 (custom-set-faces
- '(git-gutter:added    ((t (:foreground "#48d245"))))
- '(git-gutter:modified ((t (:foreground "#eedd82"))))
- '(git-gutter:deleted  ((t (:foreground "#d27490")))))
+ '(git-gutter:added    ((t (:foreground "#40883F"))))
+ '(git-gutter:modified ((t (:foreground "#AF8700"))))
+ '(git-gutter:deleted  ((t (:foreground "#FF5555")))))
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -43,7 +43,7 @@
     ((sequence "TODO(t)" "ONDECK(o)" "WAITING(w)" "SOMEDAY(s)" "CURRENT(c)" "|" "DONE(d)"))))
  '(package-selected-packages
    (quote
-    (doom-modeline 0blayout plan9-theme nofrils-acme-theme diminish base16-theme avk-emacs-themes hexrgb ipython elpy package-build ggtags helm-gtags company-dict company-emacs-eclim company company-go company-web helm-company go-mode git-gutter leuven-theme spacemacs-theme gruvbox-theme fm anaphora python-mode eldoc-eval doom-themes hl-line+ pydoc all-the-icons neotree evil-multiedit evil-anzu iedit anzu fountain-mode rainbow-mode coffee-mode yasnippet yaml-mode web-mode use-package unicode-fonts unbound smooth-scrolling robe rainbow-delimiters powerline-evil org-grep org-bullets markdown-mode key-chord helm-projectile flymake-ruby flycheck fill-column-indicator evil-visual-mark-mode evil-surround evil-snipe evil-mu4e evil-magit evil-leader evil-commentary engine-mode auto-complete)))
+    (ghub doom-modeline 0blayout plan9-theme nofrils-acme-theme diminish base16-theme avk-emacs-themes hexrgb ipython elpy package-build ggtags helm-gtags company-dict company-emacs-eclim company company-go company-web helm-company go-mode git-gutter leuven-theme spacemacs-theme gruvbox-theme fm anaphora python-mode eldoc-eval doom-themes hl-line+ pydoc all-the-icons neotree evil-multiedit evil-anzu iedit anzu fountain-mode rainbow-mode coffee-mode yasnippet yaml-mode web-mode use-package unicode-fonts unbound smooth-scrolling robe rainbow-delimiters powerline-evil org-grep org-bullets markdown-mode key-chord helm-projectile flymake-ruby flycheck fill-column-indicator evil-visual-mark-mode evil-surround evil-snipe evil-mu4e evil-magit evil-leader evil-commentary engine-mode auto-complete)))
  '(python-shell-completion-native-disabled-interpreters (quote ("pypy" "python3")))
  '(safe-local-variable-values
    (quote
@@ -56,6 +56,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(git-gutter:added ((t (:foreground "#48d245"))))
- '(git-gutter:deleted ((t (:foreground "#d27490"))))
- '(git-gutter:modified ((t (:foreground "#eedd82")))))
+ '(git-gutter:added ((t (:foreground "#40883F"))))
+ '(git-gutter:deleted ((t (:foreground "#FF5555"))))
+ '(git-gutter:modified ((t (:foreground "#AF8700")))))
